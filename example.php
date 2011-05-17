338e453005b4f7701c625ecb8630190206ad889a
vilhelmk <vilhelmkv@gmail.com>
2011-05-17 07:10:32 +0000

    SpotifyAlbum::getYear(), SpotifyAlbum::getArtist()

--- example.php
+++ example.php
@@ -9,8 +9,9 @@ $playlists = $s->getPlaylists();
 
 $pl = $playlists[7];
 
-printf("\tPlaylist name: %s\n", $pl);
+printf("\n\tPlaylist name: %s\n", $pl);
 printf("\tOwner: %s\n", $pl->getOwner());
+printf("\tCollaborative: %s\n", $pl->isCollaborative() ? 'yes' : 'no');
 
 //if (!$playlists[7]->rename("hove 2010")) {
 //	printf("rename failed\n");
@@ -21,10 +22,13 @@ echo "\n";
 $tracks = $playlists[7]->getTracks();
 foreach ($tracks as $track) {
 	$duration = $track->getDuration();
-	printf("%s - %s (album: %s, #%d) [%02d:%02d] %s [%d%%]\n",
+	$album = $track->getAlbum();
+
+	printf("%s - %s (album: %s, year: %d, #%d) [%02d:%02d] %s [%d%%]\n",
 			$track->getArtist(),
 			$track,
-			$track->getAlbum(),
+			$album,
+			$album->getYear(),
 			$track->getIndex(),
 			$duration / 60,
 			$duration % 60,
